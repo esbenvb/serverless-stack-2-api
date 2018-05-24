09159f86dfdcd3e7b77d5aedbd8b548fbde095b5
Esben von Buchwald <esben@vonbuchwald.dk>
2018-05-25 01:51:48 +0200

    add garbage code

--- create.js
+++ create.js
@@ -1,6 +1,7 @@
 import uuid from "uuid";
 import * as dynamoDbLib from "./libs/dynamodb-lib";
 import { success, failure } from "./libs/response-lib";
+røv pis lort
 
 export async function main(event, context, callback) {
   const data = JSON.parse(event.body);
